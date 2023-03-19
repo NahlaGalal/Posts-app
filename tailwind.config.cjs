79be5345700cd23fe56fe881ee92e8db5f0425bc
NahlaGalal <nahlaglal@gmail.com>
2023-03-19 05:32:44 +0200

    Add base colors and font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainColor: "#6941C6",
+        lightViolet: "#F9F5FF",
+        darkVoilet: "#42307D",
+        blackColor: "#101828",
+        greyColor: "#667085",
+      },
+      fontFamily: {
+        inter: "Inter, sans-serif",
+      },
+    },
   },
   plugins: [],
 };
