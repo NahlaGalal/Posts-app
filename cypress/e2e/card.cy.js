737370a5779b345bd0cef53b245e904d2dde0f2a
NahlaGalal <nahlaglal@gmail.com>
2023-03-20 02:15:22 +0200

    Add test case to test navigation to past page

--- cypress/e2e/card.cy.js
+++ cypress/e2e/card.cy.js
@@ -11,20 +11,30 @@ describe("Post card functionality", () => {
       const [post] = posts;
 
       // Check for post title
-      cy.get("[data-cy='post-title']").contains(post.title);
+      cy.get("[data-cy='post-title']").first().contains(post.title);
 
       // Check for post body
-      cy.get("[data-cy='post-body']").contains(post.body.slice(0, 10));
+      cy.get("[data-cy='post-body']").first().contains(post.body.slice(0, 10));
 
-      cy.get("@usersFakeData").then(users => {
-        const user = users.find(user => user.id === post.userId);
+      cy.get("@usersFakeData").then((users) => {
+        const user = users.find((user) => user.id === post.userId);
 
         // Check for user name
-        cy.get("[data-cy='user-name']").contains(user.name);
+        cy.get("[data-cy='user-name']").first().contains(user.name);
 
         // Check for user avatar
-        cy.get("[data-cy='user-avatar']").contains(user.name[0])
-      })
+        cy.get("[data-cy='user-avatar']").first().contains(user.name[0]);
+      });
+    });
+  });
+
+  it("should navigate to blog page correctly", () => {
+    cy.get("@postsFakeData").then((posts) => {
+      const [post] = posts;
+      
+      // Check navigation to post page after clicking on the link icon
+      cy.get("[data-cy='post-link']").first().click();
+      cy.location("pathname", `/post/${post.id}`);
     });
   });
 });
