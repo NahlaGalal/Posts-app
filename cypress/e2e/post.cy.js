113011af1cc998e7f5a4bd42c918eb34dbd3371b
NahlaGalal <nahlaglal@gmail.com>
2023-03-21 01:39:14 +0200

    Add test case to add comment functionality

--- cypress/e2e/post.cy.js
+++ cypress/e2e/post.cy.js
@@ -17,6 +17,24 @@ describe("Render post details page successfully", () => {
 
   beforeEach(() => {
     cy.visit("/post/1");
+    cy.clock();
+
+    // spying and response stubbing
+    // Simulate api request to post comment
+    cy.intercept(
+      "POST",
+      "https://jsonplaceholder.typicode.com/posts/1/comments",
+      {
+        status: 200,
+        data: {
+          body: "TestBody",
+          email: "test@test.com",
+          id: 501,
+          name: "Test",
+          postId: "1",
+        },
+      }
+    ).as("submitComment");
   });
 
   it("should render the UI successfully", () => {
@@ -92,5 +110,55 @@ describe("Render post details page successfully", () => {
     cy.get("[data-cy='comment-textarea']").should("have.css", "height", "64px");
     cy.get("[data-cy=submit-comment]").should("not.exist");
     cy.get("[data-cy=cancel-comment]").should("not.exist");
+  });
+
+  it("should add a comment successfully", () => {
+    cy.get("[data-cy='input-name']").focus();
+
+    // Add inputs data
+    cy.get("[data-cy='input-name']").type("Test");
+    cy.get("[data-cy='input-email']").type("test@test.com");
+    cy.get("[data-cy='comment-textarea']").type("TestBody");
+
+    // Click submit
+    cy.get("[data-cy='submit-comment']").click();
+    cy.wait("@submitComment").then((data) => {
+      expect(data.request.body.name).to.eq("Test");
+      expect(data.request.body.email).to.eq("test@test.com");
+      expect(data.request.body.body).to.eq("TestBody");
+    });
+
+    // Comment success
+    cy.get("[data-cy='comment-success']").should(
+      "contain",
+      "Comment added successfully"
+    );
+    // Wait 1 seond
+    cy.tick(1000);
+    // Comment success hidden
+    cy.get("[data-cy='comment-success']").should("not.exist");
+  });
+
+  it("should validate add comment", () => {
+    cy.get("[data-cy='comment-textarea']").focus();
+    cy.get("[data-cy='submit-comment']").click();
+
+    // Show required error messages
+    cy.get("[data-cy='name-error']").contains("Name input is required");
+    cy.get("[data-cy='email-error']").contains("Email input is required");
+    cy.get("[data-cy='comment-error']").contains("Message input is required");
+
+    // Show valid messages for email
+    cy.get("[data-cy='input-email']").type("test");
+    cy.get("[data-cy='email-error']").contains("This email is unvalid");
+
+    // Error message shoulf be hidden
+    cy.get("[data-cy='input-name']").type("Test");
+    cy.get("[data-cy='input-email']").type("test@test.com");
+    cy.get("[data-cy='comment-textarea']").type("TestBody");
+
+    cy.get("[data-cy='name-error']").should("not.exist");
+    cy.get("[data-cy='email-error']").should("not.exist");
+    cy.get("[data-cy='comment-error']").should("not.exist");
   });
 });
