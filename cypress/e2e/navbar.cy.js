81caaaa13d04deaff66a21e5d0a1a8728a36cfd7
NahlaGalal <nahlaglal@gmail.com>
2023-03-20 03:33:07 +0200

    Add test suite to navbar

---
+++ cypress/e2e/navbar.cy.js
@@ -0,0 +1,25 @@
+/// <reference types="cypress" />
+
+describe("Navbar rendered successfully", () => {
+  it("should be render in all pages except Posts(Home) page", () => {
+    // Check it does not exist in posts page
+    cy.visit("/");
+    cy.get("[data-cy='navbar']").should("not.exist");
+
+    // Check it exists in blog posts page
+    cy.visit("/post/1");
+    cy.get("[data-cy='navbar']").should("exist");
+
+    // Check it exists in blog posts page
+    cy.visit("/user/1");
+    cy.get("[data-cy='navbar']").should("exist");
+  });
+
+  it("should logo navigates to home page", () => {
+    cy.visit("/post/1");
+
+    // Click on the logo
+    cy.get("[data-cy='logo']").click();
+    cy.location("pathname", "/");
+  });
+});
