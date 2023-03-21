8a5019208c764d504ea02d1468d8ce34f90b03a5
NahlaGalal <nahlaglal@gmail.com>
2023-03-21 05:09:33 +0200

    Remove search input from navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -10,22 +10,6 @@ const Navbar: React.FC = () => {
         <Link to="/" className="text-darkVoilet text-2xl font-bold" data-cy="logo">
           Logo
         </Link>
-
-        {/* Search input */}
-        <div className="relative max-w-full">
-          <input
-            type="search"
-            placeholder="Search"
-            name="search"
-            data-cy="search-input"
-            className="[ max-w-full sm:max-w-xs ]
-          [ pr-4 pl-11 py-3 ]
-          [ text-greyColor rounded-lg ]"
-          />
-          <span className="absolute top-3 left-4">
-            <FiSearch className="text-greyColor w-5 h-5" />
-          </span>
-        </div>
       </nav>
     </header>
   );
