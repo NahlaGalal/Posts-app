39b39e78e98d1713375dd0ab649ccdd5f56eb65b
NahlaGalal <nahlaglal@gmail.com>
2023-03-21 02:00:26 +0200

    Split comment to separate component

---
+++ src/components/Details/Comment.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { IComment } from "./Types";
+
+const Comment: React.FC<IComment> = ({ body, email, id, name }) => {
+  return (
+    <div key={id} className="flex gap-4" data-cy="comment">
+      {/* Commenter avatar */}
+      <span
+        className="[ flex justify-center items-center ]
+                [ w-10 h-10 mt-4 ] 
+                [ rounded-full bg-darkVoilet text-white ]
+                [ font-bold text-xl ]"
+        data-cy="commenter-avatar"
+      >
+        {name[0].toLocaleUpperCase()}
+      </span>
+
+      <div
+        className="[ border border-greyColor border-opacity-20 rounded ] 
+            flex-1 p-4"
+      >
+        {/* Commenter name */}
+        <p className="text-mainColor text-lg" data-cy="commenter-name">
+          {name}
+        </p>
+
+        {/* Commenter email */}
+        <p className="text-greyColor text-sm mb-4" data-cy="commenter-email">
+          {email}
+        </p>
+
+        {/* Comment body */}
+        <p data-cy="comment-body">{body}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Comment;
--- src/components/Details/Comments.tsx
+++ src/components/Details/Comments.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import AddCommentUI from "./AddCommentUI";
+import Comment from "./Comment";
 import { ICommentsProps } from "./Types";
 
 const Comments: React.FC<ICommentsProps> = ({ comments }) => {
@@ -18,39 +19,7 @@ const Comments: React.FC<ICommentsProps> = ({ comments }) => {
       {/* List all comments */}
       <div className="flex flex-col gap-6">
         {comments.map((comment) => (
-          <div key={comment.id} className="flex gap-4" data-cy="comment">
-            {/* Commenter avatar */}
-            <span
-              className="[ flex justify-center items-center ]
-                [ w-10 h-10 mt-4 ] 
-                [ rounded-full bg-darkVoilet text-white ]
-                [ font-bold text-xl ]"
-              data-cy="commenter-avatar"
-            >
-              {comment.name[0].toLocaleUpperCase()}
-            </span>
-
-            <div
-              className="[ border border-greyColor border-opacity-20 rounded ] 
-            flex-1 p-4"
-            >
-              {/* Commenter name */}
-              <p className="text-mainColor text-lg" data-cy="commenter-name">
-                {comment.name}
-              </p>
-
-              {/* Commenter email */}
-              <p
-                className="text-greyColor text-sm mb-4"
-                data-cy="commenter-email"
-              >
-                {comment.email}
-              </p>
-
-              {/* Comment body */}
-              <p data-cy="comment-body">{comment.body}</p>
-            </div>
-          </div>
+          <Comment key={comment.id} {...comment} />
         ))}
       </div>
     </section>
