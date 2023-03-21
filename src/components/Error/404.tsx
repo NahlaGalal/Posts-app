f648ab6ce66671b88a3efb9c76738616dd3795f1
NahlaGalal <nahlaglal@gmail.com>
2023-03-21 19:56:24 +0200

    Change position of image

--- src/components/Error/404.tsx
+++ src/components/Error/404.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "../Navbar";
-import ErrorImg from "../../../public/error-404.svg";
+import ErrorImg from "../../images/error-404.svg";
 import { Link } from "react-router-dom";
 
 const Error404: React.FC = () => {
--- src/components/Error/index.tsx
+++ src/components/Error/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "../Navbar";
-import ErrorImg from "../../../public/err.svg";
+import ErrorImg from "../../images/err.svg";
 import { Link } from "react-router-dom";
 
 const Error: React.FC = () => {
